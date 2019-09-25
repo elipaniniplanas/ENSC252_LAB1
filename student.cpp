9452ab59ebfaa292c6e08ff131e4633a251b282c
Eli Planas <43528404+elipaniniplanas@users.noreply.github.com>
2019-09-25 10:30:23 -0700

    Update student.cpp

--- student.cpp
+++ student.cpp
@@ -2,9 +2,94 @@
 #include "student.hpp"
 #include <iostream>
 
+//ToeflScore constructor
+ToeflScore::ToeflScore(int read, int write, int listen, int speak)
+{
+        if(read <0 || read > 30)
+        {
+                cout << "sorry, your reading toeflScore is invalid";
+
+        }
+
+        if(write <0 || write > 30)
+        {
+                cout << "sorry, your writing toeflScore is invalid";
+
+        }
+
+        if(listen <0 || listen > 30)
+        {
+                cout << "sorry, your listening toeflScore is invalid";
+
+        }
+
+        if(speak <0 || speak > 30)
+        {
+                cout << "sorry, your speaking toeflScore is invalid";
+
+        }
+        reading = read;
+        writing = write;
+        listening = listen;
+        speaking = speak;
+        totalscore = read + write + listen + speak;
+}
+ToeflScore::ToeflScore();
+{
+        reading = 0;
+        writing = 0;
+        listening = 0;
+        speaking = 0;
+        totalscore = 0;
+}
+//Below are the mutator functions for the ToeflScore class
+void ToeflScore::setreading(int read)
+{
+        reading = read;
+}
+void ToeflScore::setwriting(int write)
+{
+        writing = write;
+}
+void ToeflScore::setlistening(int listen)
+{
+        listening = listen;
+}
+void ToeflScore::setspeaking(int speak)
+{
+        speaking = speak;
+}
+//This sets the sum of all of the scores in a ToeflScore object
+void ToeflScore::settotalscore()
+{
+        totalscore = reading + writing + listening + speaking;
+}
+//Below are the accessor functions for the ToeflScore class
+int ToeflScore::getreading()
+{
+        return(reading);
+}
+int ToeflScore::getwriting()
+{
+        return(writing);
+}
+int ToeflScore::getlistening()
+{
+        return(listening);
+}
+int ToeflScore::getspeaking()
+{
+        return(speaking);
+}
+int ToeflScore::gettotalscore()
+{
+        return(totalscore);
+}
+
 //Student constructor
 STUDENT::STUDENT(string first, string last, float cgpa, int score, int id)
 {
+        //Error checking, making sure that the 
         if(cgpa > 4.3 || cgpa < 0)
         {
                 cout<<"sorry, but your CGPA is invalid";
@@ -62,81 +147,6 @@ int STUDENT::getscore()
 int STUDENT::getID()
 {
         return(ID);
-}
-//ToeflScore constructor
-ToeflScore::ToeflScore(int read, int write, int listen, int speak)
-{
-        if(read <0 || read > 30)
-        {
-                cout << "sorry, your reading toeflScore is invalid";
-
-        }
-
-        if(write <0 || write > 30)
-        {
-                cout << "sorry, your writing toeflScore is invalid";
-
-        }
-
-        if(listen <0 || listen > 30)
-        {
-                cout << "sorry, your listening toeflScore is invalid";
-
-        }
-
-        if(speak <0 || speak > 30)
-        {
-                cout << "sorry, your speaking toeflScore is invalid";
-
-        }
-        reading = read;
-        writing = write;
-        listening = listen;
-        speaking = speak;
-        totalscore = read + write + listen + speak;
-}
-//Below are the mutator functions for the ToeflScore class
-void ToeflScore::setreading(int read)
-{
-        reading = read;
-}
-void ToeflScore::setwriting(int write)
-{
-        writing = write;
-}
-void ToeflScore::setlistening(int listen)
-{
-        listening = listen;
-}
-void ToeflScore::setspeaking(int speak)
-{
-        speaking = speak;
-}
-//This sets the sum of all of the scores in a ToeflScore object
-void ToeflScore::settotalscore()
-{
-        totalscore = reading + writing + listening + speaking;
-}
-//Below are the accessor functions for the ToeflScore class
-int ToeflScore::getreading()
-{
-        return(reading);
-}
-int ToeflScore::getwriting()
-{
-        return(writing);
-}
-int ToeflScore::getlistening()
-{
-        return(listening);
-}
-int ToeflScore::getspeaking()
-{
-        return(speaking);
-}
-int ToeflScore::gettotalscore()
-{
-        return(totalscore);
 }
 //Constructor for the DomesticStudent class
 DomesticStudent::DomesticStudent(string first, string last, float cgpa, int score, int id, string pv):
